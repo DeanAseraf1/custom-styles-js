8345c31936315ee046cea95b77f9c25950b04cb0
Dean Yair Aseraf <90892852+DeanAseraf1@users.noreply.github.com>
2023-09-02 10:24:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,148 @@
+const customStyles = (function() {
+
+    //global-properties
+    const attributeName = "custom-style"
+    const refAttributeName = "custom-style-ref"
+    const stylesheetName = "custom-stylesheet"
+    const customStylesReferences= []
+
+    const getCustomStyleRef = (key) =>{
+        const currentCustomStyleRef = customStylesReferences.find(item=>item.key === key)
+        if(!currentCustomStyleRef) 
+            return null
+
+        return currentCustomStyleRef;
+    }
+    
+    const getCustomStylesheetRule = (customStyleRef) => {
+        const currentCustomStyleRef = getCustomStyleRef(customStyleRef)
+        if(!currentCustomStyleRef) {
+            console.warn(`custom-style-ref="${customStyleRef}" is not defined.\nTry adding it to the refered element.`)
+            return null;
+        }
+        
+        const customSheetRule = [...customStyleSheet.sheet.cssRules].find(item=>item.selectorText === `.${currentCustomStyleRef.value}`)
+        if(!customSheetRule) {
+            console.warn(`The css class ".${currentCustomStyleRef.value}" wasn't found.\nAre you sure you added a proper reference?`)
+            return null;
+        }
+        
+        return customSheetRule;
+    }
+
+    const updateRefrencesInCSS = (cssText) =>{
+        for(let i = 0; i< customStylesReferences.length; i++){
+            const currentCustomStyleRef = getCustomStyleRef(customStylesReferences[i].key)
+            if(cssText.includes(`[${currentCustomStyleRef.key}]`)){
+                cssText = cssText.replaceAll(`[${currentCustomStyleRef.key}]`, `.${currentCustomStyleRef.value}`)
+            }
+        }
+        return cssText;
+    }
+
+    const customStyleSheet = (function(){
+        const customStyleSheet = document.createElement("style")
+        customStyleSheet.setAttribute("id", stylesheetName)
+        document.head.appendChild(customStyleSheet)
+
+        //handaling main custom-styles
+        const elements = document.querySelectorAll(`[${attributeName}]`)
+            clearWordRegex = /[\s]+/g
+            for(let i = 0; i < elements.length; i++){
+                const attributeValue = elements[i].getAttribute(attributeName)
+                const newCssClassName = `${attributeName}-${i}`
+                const newCssRule = `.${newCssClassName} {${updateRefrencesInCSS(attributeValue)}\n}\n\n`
+                //const newCssRule = `.${newCssClassName} {${attributeValue}\n}\n\n`
+                //customStyleSheet.innerHTML += newCssRule
+                customStyleSheet.sheet.insertRule(newCssRule, i)
+                elements[i].removeAttribute(attributeName)
+                elements[i].classList.add(newCssClassName)
+                
+                if(elements[i].hasAttribute(refAttributeName)){
+                    const key = elements[i].getAttribute(refAttributeName)
+                    const currentCustomStyleRef = getCustomStyleRef(key);
+                    if(currentCustomStyleRef){
+                        console.warn(`custom-style-ref="${key} is already declared."\nPlease use different value.`)
+                        return
+                    }
+                    
+                    customStylesReferences.push({key: key, value: newCssClassName})
+                    elements[i].removeAttribute(refAttributeName)
+                }
+            }
+        
+        //handaling custom-style-refs
+        const referenceElements = document.querySelectorAll(`[${refAttributeName}]`)
+            for(let i = 0; i < referenceElements.length; i++){
+                customStyleRef = referenceElements[i].getAttribute(refAttributeName)
+                const customStyle = getCustomStyleRef(customStyleRef)
+                if(!customStyle){
+                    console.warn(`custom-style-ref="${customStyleRef}" is not defined.\nTry adding it to the refered element.\nAnd check that it uses a custom-style attribute.`)
+                    return
+                }
+                referenceElements[i].classList.add(customStyle.value)
+                referenceElements[i].removeAttribute(refAttributeName)
+            }
+        
+        return customStyleSheet
+    })()
+
+    //global functions
+    //post-render custom-style update
+    /**
+     * @param customStyleRef Defined custom-style-sheet-ref attribute value.
+     * @param propertyKey Property name to set on the custom-style.
+     * @param propertyValue Property value to set on the custom-style.
+     */
+    const updateCustomStyle = (customStyleRef, propertyKey, propertyValue) => {
+        const customRule = getCustomStylesheetRule(customStyleRef)
+        if(!customRule)
+            return;
+        
+        customRule.style.setProperty(propertyKey, propertyValue)
+    }
+
+    //post-render custom-style update
+    /**
+     * @param customStylesObject an object with all updated custom properties.
+     */
+    const updateCustomStyles = (customStylesObject) => {
+        const objectKeys = Object.keys(customStylesObject)
+        for(let i = 0; i < objectKeys.length; i++){
+            const currentDefinition = Object.values(customStylesObject)[i]
+            const currentKeys = Object.keys(currentDefinition)
+            for(let j = 0; j < currentKeys.length; j++){
+                const key = currentKeys[j]
+                const value = Object.values(currentDefinition)[j]
+                updateCustomStyle(objectKeys[i], key, value)
+            }
+        }
+        
+    }
+
+    //post-render add custom rule
+    /**
+     * @param rule Rule to add to the custom stylesheet as a string.
+     * Use [] to specify a custom-style-ref.
+     */
+    const addCustomRule = (rule) => {
+        
+        customStyleSheet.sheet.insertRule(updateRefrencesInCSS(rule), customStyleSheet.sheet.cssRules.length)
+    }
+    
+    //post-render add custom rules
+    /**
+     * @param rules Rules to add to the custom stylesheet as a string.
+     * Use [] to specify a custom-style-ref.
+     */
+    const addCustomRules = (...rules) => {
+        for(let i = 0; i < rules.length; i++){
+            addCustomRule(rules[i]);
+        }
+    }
+
+    return { updateCustomStyle, updateCustomStyles, addCustomRule, addCustomRules }
+})()
+
+
+module.exports = customStyles
